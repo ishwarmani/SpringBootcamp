1f66c56c60da5da3bce87eb1d3f576736f4ed324
imthapa <ishwarmani.thapa@tothenew.com>
2019-04-29 18:36:10 +0530

    Solution for Question 6a, autowiring byName

--- SpringCore/src/main/java/com/springcore/Execute.java
+++ SpringCore/src/main/java/com/springcore/Execute.java
@@ -9,19 +9,23 @@ public class Execute {
     public static void main(String[] args) {
 
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("config.xml");
-        Database database = (Database)applicationContext.getBean("database");
+//        Database database = (Database)applicationContext.getBean("database");
 //        System.out.println(database);
 
-        Restaurant restaurant = (Restaurant) applicationContext.getBean("teaRestaurant");
+//        Restaurant restaurant = (Restaurant) applicationContext.getBean("teaRestaurant");
 //        restaurant.getHotDrink().prepareHotDrink();
 
-        Restaurant restaurant1 = (Restaurant)applicationContext.getBean("expressTeaRestaurant");
+//        Restaurant restaurant1 = (Restaurant)applicationContext.getBean("expressTeaRestaurant");
 //        restaurant1.getHotDrink().prepareHotDrink();
 
-        Complex complex = (Complex)applicationContext.getBean("complexBean");
-        System.out.println(complex);
-        System.out.println(complex.list);
-        System.out.println(complex.set);
-        System.out.println(complex.map);
+//        Complex complex = (Complex)applicationContext.getBean("complexBean");
+//        System.out.println(complex);
+//        System.out.println(complex.list);
+//        System.out.println(complex.set);
+//        System.out.println(complex.map);
+
+        Restaurant restaurant2 = (Restaurant)applicationContext.getBean("restaurant2");
+        restaurant2.getHotDrink().prepareHotDrink();
+
     }
 }
