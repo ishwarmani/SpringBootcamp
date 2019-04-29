11f0e0fd36179c5529493bc8390110656b161a82
imthapa <ishwarmani.thapa@tothenew.com>
2019-04-29 18:13:51 +0530

    Solution for Question 5 (Complex class)

--- SpringCore/src/main/java/com/springcore/Execute.java
+++ SpringCore/src/main/java/com/springcore/Execute.java
@@ -10,13 +10,18 @@ public class Execute {
 
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("config.xml");
         Database database = (Database)applicationContext.getBean("database");
-        System.out.println(database);
+//        System.out.println(database);
 
         Restaurant restaurant = (Restaurant) applicationContext.getBean("teaRestaurant");
-        restaurant.getHotDrink().prepareHotDrink();
+//        restaurant.getHotDrink().prepareHotDrink();
 
         Restaurant restaurant1 = (Restaurant)applicationContext.getBean("expressTeaRestaurant");
-        restaurant1.getHotDrink().prepareHotDrink();
+//        restaurant1.getHotDrink().prepareHotDrink();
 
+        Complex complex = (Complex)applicationContext.getBean("complexBean");
+        System.out.println(complex);
+        System.out.println(complex.list);
+        System.out.println(complex.set);
+        System.out.println(complex.map);
     }
 }
